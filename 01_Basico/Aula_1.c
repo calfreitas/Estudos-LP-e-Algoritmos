9bc3be574c875718c29108bc3096eba0bff843d4
calliel.freitas <calliel@avotech.com.br>
2025-08-19 15:59:51 -0300

    add: aula1

---
+++ 01_Basico/Aula_1.c
@@ -0,0 +1,12 @@
+// Para executar o programa, necessáriamente devemos compila-lo em um executável. 
+
+#include <stdio.h>  // Standard (std) input (i) output (o) . h (header file) utilizada para leitura / processamento e reconhecimento de dados externos 
+                    // Como por exemplo as funções printf() e fscan()
+
+int main ( ){                    // Dentro de parêntesis mantemos os ( parâmetros ) da função, e dentro de chaves { tudo o que será executado pela máquina - função } 
+    printf("Hello, World \n");   // Função pré-programada em stdio.h para printar informações em CLI + \n (subcomando para pular uma linha).
+    
+    
+}
+
+
